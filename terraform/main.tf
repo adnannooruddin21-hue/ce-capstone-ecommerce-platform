df330e2bc3c5ce1d7c4d56b35cffde710a08f56d
Adnan Nooruddin <adnan.nooruddin21@gmail.com>
2026-09-01 19:36:44 +0200

    chore: set app version to 2.0.0 (#11)

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,6 +22,7 @@ module "compute" {
   app_sg_id          = module.security.app_sg_id
   instance_type      = var.instance_type
   app_port           = var.app_port
+  app_version        = "2.0.0"
 }
 
 module "monitoring" {
